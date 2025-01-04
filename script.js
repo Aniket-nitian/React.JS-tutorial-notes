25a9f93a14f2d9be6799aaf3bd7ea387d3c4b2a1
Aniket-nitian <anik807621@gmail.com>
2025-01-04 16:40:01 +0530

    react using cdn link

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-const container = React.createElement("h2", { className: "title", id: "topic"}, "Hello Aniket from ReAcT");  //tag, attributes, content 
+// const container = React.createElement("h2", { className: "title", id: "topic"}, "Hello Aniket from ReAcT");  //tag, attributes, children 
 
-const Aniket = ReactDOM.createRoot(document.querySelector("#root"))//It create a element in object form 
-Aniket.render(container); // renders/showing the h2 element to the root element in the HTML document
+// const Aniket = ReactDOM.createRoot(document.querySelector("#root"))//It create a element in object form 
+// Aniket.render(container); // renders/showing the h2 element to the root element in the HTML document
 
 
 
-const container2 = React.createElement('section',
-  {}, 
+{/* 
+  <div id="container">
+  <h1>I am heading 1</h1>
+  <h2>I am heading 2</h2>
+</div> */
+}
+
+const containerDIV = React.createElement('div',{id:"container"},
   [
-    React.createElement('p', {key: 1}, "This is a paragraph inside a section")
-  ],
-  "HelloEveryBody Hows Going",
-  [React.createElement(
-    'form',
-    {key: 3,},
-    "Username"
-  ),
-React.createElement('input',{key: 4, htmlFor: 'username',id: 'username'})]
-)
+    React.createElement('h1',{},"I am heading 1"),
+    React.createElement('h2',{},"I am heading 2"),
+  ]
+);  //reactElement == object
 
-const root2 = ReactDOM.createRoot(document.querySelector("#root2"))
-root2.render(container2)
-
-
-const container3 = React.createElement(
-  'div',
-  {key: 1},
-  [React.createElement(
-    'img',
-    {key: 2, src: "https://w7.pngwing.com/pngs/880/805/png-transparent-react-hd-logo.png", style: {width: "200px"}},
-
-  )],
-)
-
-const root3 = ReactDOM.createRoot(document.querySelector("#root3"))
-root3.render(container3);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(containerDIV);
