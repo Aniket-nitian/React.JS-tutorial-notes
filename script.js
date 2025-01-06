30efed0f053df1631162124da1ff3ab485be79f1
Aniket-nitian <anik807621@gmail.com>
2025-01-06 16:16:14 +0530

    JSX completed

--- script.js
+++ script.js
@@ -14,32 +14,47 @@ import ReactDOM from "react-dom/client";
   <h2>I am heading 2</h2>
 </div> */
 
-
+//using without JSX
 const containerDIV = React.createElement('div',{key:0,id:"container"},
   [
     React.createElement('h1',{key:1},"I am heading 1"),
-    React.createElement('h2',{key:2},"I am heading 20"),
+    React.createElement('h2',{key:2},"I am heading 2"),
   ]
 );  //reactElement == object
 
+//using JSX
+const name = "Aniket"
+const jsxHeading = (
+  <div className="container">
+    {name}
+    <h1>I am heading from JSX</h1>  
+  </div>
+)
+console.log(jsxHeading);
+// jsx
+//using babel => JSX --> babel(transpiler) --> reactElement
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(containerDIV);
+root.render(jsxHeading);
 
 
-//Bundlers are -
-// webpack
-// Vite
-// parcel -->do import/export, create a local host, hot reload,dev server, minify the code, transpile the code, bundle the code, etc.
+//components -- normal js function.
+//function component and class based component(older one)
 
-//npm(npm init then npm i -D parcel)
-//depeandancy and devDepeandancy
+const Heading1 = () => {
+  return <h4>NICE</h4>
+}
+const Heading = () => {
+  return (
+    <div>
+      <h2>This is components</h2>
+      <Heading1/>
+      {/* or */}
+      {Heading1()}
+      <h2>Hello Aniket</h2>
+    </div>
+  );
+}
 
-//npm i react react-dom  --> now no need of react.development and react-dom.development of CDN link
-
-//npx(parcel execute) parcel index.html
-//npx parcel build index.html --> for production(after add build key in script in package.json)
-//npm run build --> for production
-
-//tilde(~) is used for installing the latest version of the package, major change
-// carate(^) is used for installing the latest version of the package which is compatible with the current version of the package, monor change in version
-// . is used for installing the current version of the package
+const component = ReactDOM.createRoot(document.querySelector("#component"));
+component.render(<Heading/>);
